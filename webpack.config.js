14082cc111235d0eb832c6e517c39e092797a796
João Fernandes <joaovitorfernandes2@gmail.com>
2018-09-05 07:28:07 -0300

    Add webpack-cli and webpack config mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
     filename: 'index.js',
